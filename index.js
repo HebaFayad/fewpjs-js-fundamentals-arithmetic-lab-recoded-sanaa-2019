e99a0416282eb4d6266e956c17d24ea70b125c1c
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-11-09 16:48:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-// Write your code here
+// Write your code herelet num1=31;let num2=2;
 let multiply=num1*num2;
-let num1=31;let num2=2;
+
 let random=Math.random();
 let mod=24%5;
 let max=Math.max(12,15,20);
